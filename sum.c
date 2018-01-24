ed63f4c10f9aec3378b8eb0572362db306c4198e
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-01-24 16:24:48 +0530

    sum.c

---
+++ sum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int v, i, sum = 0;
+    
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+
+    for(i=1; i <= v; ++i)
+    {
+ sum += i;   
+}
+printf("Sum = %d",sum);
+return 0;
+}
+
