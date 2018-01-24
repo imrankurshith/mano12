076ae2c001ae41a3b3ef6670aabb68181b5a2482
1125pathy <35588777+1125pathy@users.noreply.github.com>
2018-01-24 16:21:35 +0530

    hello.c

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main(){
+    int counter,n;
+    printf("how many times:",n);
+    scanf("%d",n);
+    for(counter = 0; counter < n; counter++){
+     printf("Hello World\n");
+    }
+    return 0;
